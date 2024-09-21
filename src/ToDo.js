ec237b86ec5cd9054ca8a7a2b9b183cea41b087c
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-09-22 00:21:42 +0530

    Function to create a new task.

---
+++ src/ToDo.js
@@ -0,0 +1,30 @@
+const ToDoController = ()=>{
+    let tasks = [{
+        id: 67890,
+        title: "Get Better", 
+        description: "All nighter",
+        dueDate: "03/03/2024",
+        priority: "High",
+        project : {
+            id: 12344,
+            name: "Default"
+        }
+    }
+    ];
+
+    const createTask = (title, description, dueDate, priority, project)=>{
+        const task = {
+            id: Date.now(),
+            title,
+            description,
+            dueDate,
+            priority,
+            project: {
+                id: project.id,
+                name: project.name,  
+            }            
+        };
+        tasks.push(task);
+        saveTasksToStorage();
+    }
+}
