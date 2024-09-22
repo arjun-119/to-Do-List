cc7046d04816f1186cef31ad7d35565de3a39c9b
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-09-22 15:28:31 +0530

    functions to add task into project after task creation and to delete task from project.

--- src/Project.js
+++ src/Project.js
@@ -31,20 +31,53 @@ const projectController = () => {
     return projects;
   };
 
+  const addTaskToProject = (id, task)=>{
+      const updateProject = projects.find(project=> project.id === id);
+
+      const taskInProject = updateProject.find(toDo=> toDo.id === task.id)
+
+      if(taskInProject){
+        const taskIndex = updateProject.tasks.findIndex(index=> index.id === task.id);
+        updateProject.tasks[taskIndex] = task;
+      }
+      else
+      {
+        updateProject.tasks.push(task);
+      }
+      saveProjectToStorage();
+  }
+
+  const getProjects = ()=> projects;
+
   const deleteProject = (id) => {
     const toDelete = projects.findIndex((project) => project.id === id);
     projects.splice(toDelete, 1);
 
+    taskController()
+      .getTask()
+      .forEach((task) => taskController().deleteTask(task.id));
+
+    saveProjectToStorage();
+  };
+
+  const deleteTaskfromProject = (id, task) => {
+    const ProjectToUpdate = projects.find((project) => project.id === id);
+    const taskIndex = ProjectToUpdate.findIndex(
+      (element) => element.id === task.id
+    );
+    ProjectToUpdate.tasks.splice(taskIndex, 1);
     saveProjectToStorage();
   };
 
   return {
     createProject,
+    addTaskToProject,
+    getProjects,
     saveProjectToStorage,
     getProjectFromStorage,
     deleteProject,
+    deleteTaskfromProject,
   };
 };
-
 
 export default projectController();
